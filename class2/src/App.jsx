993550950507ba02782bbdc6e383b5ea8f747919
atholehansraj <hansrajathole1@gmail.com>
2024-12-06 19:15:54 +0530

    SectionThee done

--- class2/src/App.jsx
+++ class2/src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./component/Navbar.jsx";
 import SectionOne from "./component/SectionOne.jsx";
 import SectionTwo from "./component/SectionTwo.jsx";
+import SectionThree from "./component/SectionThree.jsx";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Navbar />
       <SectionOne/>
       <SectionTwo/>
+      <SectionThree/>
     </div>
   );
 }
---
+++ class2/src/component/SectionThree.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const SectionThree = () => {
+  return (
+    <div className="mt-14 w-full px-12">
+      <div className="flex flex-col justify-center items-center relative">
+        <p>Cases</p>
+        <h1 className="text-9xl font-bold uppercase mt-16">Head Turning</h1>
+        <h1 className="text-9xl font-bold uppercase"> Projects </h1>
+        <button className="bg-yellow-300 p-4 font-semibold text-xl rounded-[1.2em] absolute left-[60%] top-[93%] rotate-[-10deg] ">Gotta see'm all</button>
+      </div>
+      <div className=" w-full flex justify-end mt-5">
+        <button className="px-5  py-4 font-semibold border-2 border-black rounded-[1em] ">Alle projecten <i class="ri-arrow-right-line ml-3"></i></button>
+      </div>
+    </div>
+  );
+};
+
+export default SectionThree;
