88be1b58df0a918d3ee040e9d95daf120655bd87
atholehansraj <hansrajathole1@gmail.com>
2024-12-30 16:40:46 +0530

    home page done

--- Sheryians/src/components/Navbar.jsx
+++ Sheryians/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import logo from '../assets/Sheryians_logo.png';
 const Navbar = () => {
 
   return (
-    <div className='flex w-full  justify-between items-center text-center  px-6 py-3 bg-gray-600 font-semibold text-white'>
+    <div className='flex w-full fixed justify-between items-center text-center  px-14 py-3 backdrop-blur-lg font-semibold text-white'>
         <div className='flex gap-2 items-center'>
             <img src={logo} alt="" className='h-7 w-5 '/>
             <h1>Sheryians coding school</h1>
@@ -15,7 +15,7 @@ const Navbar = () => {
             <Link to="/courses">Courses</Link>
             <Link to="/kodr">Bootcapm(Kodr)</Link>
             <Link to="/request">Request callBack</Link>
-            <Link to="/signin" className='bg-teal-600 p-1 px-2 rounded-md'>Sign in</Link>
+            <Link to="/signin" className='bg-[#27DFB3] text-black p-1 px-5 rounded-md'>Sign in</Link>
         </div>
     </div>
   )
--- Sheryians/src/pages/Home.jsx
+++ Sheryians/src/pages/Home.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className=' h-screen 
+     flex flex-col justify-center items-center text-center'>
+      <div className='w-[60%] h-[50%] text-8xl font-light'>
+        <h1>We only <span className='text-[#24CFA6]' >tech</span></h1>
+        <h1>what we are really</h1>
+        <h1>really <span className='italic'>good</span> at.</h1>
+        <Link className='text-xl bg-[#27DFB3] px-9 py-2 rounded-md text-black font-semibold' to="/courses">Check Courses-make an impact</Link>
+      </div>
+    
+    </div>
   )
 }
 
